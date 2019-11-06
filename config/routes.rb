15e7361910597597cdda3a9900e4fc91eb47cac2
Ubuntu <ubuntu@ip-172-31-85-69.ec2.internal>
2019-11-06 13:09:23 +0000

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'admin/admin'
   get "login" => "sessions#login"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
