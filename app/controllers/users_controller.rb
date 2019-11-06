21c33aacc97a839d5ec81d3215ed89f6f8e94b29
Pham Hoang Long <phamlong15297@gmail.com>
2019-11-06 19:13:25 +0700

    ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,12 @@ class UsersController < ApplicationController
     # render :template => '/users/customs/index', :locals => {:users => User.all}
     render :template => '/users/customs/index'
   end
+
+  def create
+    # user = User.find_by name: params[:]
+    # abort params[:email].inspect
+    # sql = "INSERT INTO users(email,password_digest,updated_at,created_at) VALUES ('a@gmail.com','log','1970-01-02','1970-01-02')"
+    # records_array = ActiveRecord::Base.connection.execute(sql)
+    return :template => '/users/customs/index'
+  end
 end
