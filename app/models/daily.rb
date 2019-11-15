44d6800e70e3f1293968eab461918f815143c139
Ubuntu <ubuntu@ip-172-31-85-69.ec2.internal>
2019-11-15 03:09:01 +0000

    show restaurant

--- app/models/daily.rb
+++ app/models/daily.rb
@@ -2,8 +2,8 @@ class Daily < ApplicationRecord
     belongs_to :restaurant
     has_many :daily_foods
   
-    validates :start_time_order, presence: true
-    validates :end_time_order, presence: true, date: {after: :start_time_order}
+    # validates :start_time_order, presence: true
+    # validates :end_time_order, presence: true, date: {after: :start_time_order}
   
     class << self
       def today
