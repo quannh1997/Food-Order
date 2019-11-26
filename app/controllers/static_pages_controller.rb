692d6527fbce75e1f3d2b77c9cbcb1d50b3f2b56
Pham Hoang Long <phamlong15297@gmail.com>
2019-11-26 23:11:18 +0700

    thêm trang chứa theme

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
+  # layout "inventory"
   def home
+    if !user_signed_in?
+      # render :template => "theme/index", :locals => {}
+      render file: "#{Rails.root}/app/views/theme/full-theme.html.erb", layout: false
+      # render layout: "theme/full-theme"
+    end
   end
 end
